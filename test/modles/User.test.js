ebd103411ffec081ba6b64931fa7d47f420976ff
Michael Hayes <michael.hayes@urbanairship.com>
2014-03-02 01:01:41 -0800

    removed setTimeout I was using for testing

--- test/modles/User.test.js
+++ test/modles/User.test.js
@@ -1,20 +1,17 @@
 var init_user = require('../../lib/models/User')
   , config = require('../../lib/config.json')
   , backend = require('../backend')
-  , test = require('..')
+  , test = require('../index')
 
 config.backend = backend
 
 test('can init User', function(t) {
   var User = init_user(config)
 
-  t.plan(5)
-  setTimeout(function() {
-    t.ok(User, 'returns an object')
-    t.equal(typeof User.find, 'function', 'User has a find method')
-    t.equal(typeof User.create, 'function', 'User has a create method')
-    t.equal(typeof User.update, 'function', 'User has a update method')
-    t.equal(typeof User.auth, 'function', 'User has a auth method')
-    t.end()
-  }, 500)
+  t.ok(User, 'returns an object')
+  t.equal(typeof User.find, 'function', 'User has a find method')
+  t.equal(typeof User.create, 'function', 'User has a create method')
+  t.equal(typeof User.update, 'function', 'User has a update method')
+  t.equal(typeof User.auth, 'function', 'User has a auth method')
+  t.end()
 })
