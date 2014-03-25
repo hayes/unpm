472343d8077bc1c8390435e5e4feee27e481e5b9
AWinterman <andywinterman@gmail.com>
2014-03-24 17:07:33 -0700

    tests work!

--- test/handler.test.js
+++ test/handler.test.js
@@ -1,16 +1,23 @@
 var CLS = require('continuation-local-storage')
-  , unpm = CLS.createNamespace('unpm')
   , Router = require('unpm-router')
-  , http = require('http')
-  , t = require('assert')
   , log = require('npmlog')
+  , http = require('http')
+  , test = require('tape')
+
+log.level = 'silent'
+
+var unpm = CLS.createNamespace('unpm')
 
 var handler = require('../lib/handler')
-  , test = require('tape')
 
 unpm.run(function() {
   unpm.set('log', log)
-  req_and_resp_saved(t)
+  test('req and resp saved', req_and_resp_saved)
+})
+
+unpm.run(function() {
+  unpm.set('log', log)
+  test('not found and 500 works', errors_work_as_expected)
 })
 
 function req_and_resp_saved(t) {
@@ -18,14 +25,14 @@ function req_and_resp_saved(t) {
     , options
     , server
 
-  router.add('GET', 'arbitrary/path', arbitrary_handler)
+  router.add('GET', '/arbitrary/path', arbitrary_handler)
 
-  function arbitrary_handler(req, resp, route, respond) {
-    console.log('handler called')
+  function arbitrary_handler(req, res, route, respond) {
 
     t.equal(req, unpm.get('req'))
     t.equal(res, unpm.get('res'))
-    t.equal(route, 'route')
+    t.equal(route, unpm.get('route'))
+    t.equal(route.route, '/arbitrary/path')
 
     respond(null, 200, {arbitrary: 'data'})
   }
@@ -37,24 +44,75 @@ function req_and_resp_saved(t) {
   server.listen(8910, on_listen)
 
   function on_listen() {
-    console.log('listening')
-
     options = {
         port: 8910
       , method: 'GET'
-      , path: 'arbitrary/path'
+      , path: '/arbitrary/path'
       , host: 'localhost'
     }
 
-    req = http.request(options, function(res) {
-      console.log('got response!')
-
-      t.end()
+    var req = http.request(options, function(res) {
+      server.close(t.end.bind(t))
     })
 
-    console.log('making request')
     req.write('hi')
     req.end()
-    console.log('request made')
+  }
+}
+
+function errors_work_as_expected(t) {
+  t.plan(2)
+
+  var router = Router()
+    , options
+    , server
+
+  router.add('GET', '/500', handler_500)
+
+  function handler_500(req, res, route, respond) {
+    respond(new Error('woo!'))
+  }
+
+  unpm.set('router', router)
+
+  server = http.createServer(handler)
+
+  server.listen(8910, 'localhost', on_listen)
+
+  function on_listen() {
+    options = {
+        port: 8910
+      , method: 'GET'
+      , path: '/500'
+      , host: 'localhost'
+    }
+
+    var req = http.request(options, function(res) {
+
+      t.strictEqual(res.statusCode, 500)
+
+      next_request()
+    })
+
+    req.write('hi')
+    req.end()
+  }
+
+  function next_request() {
+    options = {
+        port: 8910
+      , method: 'GET'
+      , path: '/'
+      , host: 'localhost'
+    }
+
+    var req = http.request(options, function(res) {
+      t.strictEqual(res.statusCode, 404)
+
+      server.close(t.end.bind(t))
+    })
+
+    req.write('hi')
+    req.end()
   }
 }
