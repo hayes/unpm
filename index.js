e35fa1682376576e85daf18ee0d710f68103acdc
jesse keane <me@jessekeane.me>
2014-03-27 09:48:50 -0700

    change order of config load

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var backend = require('unpm-fs-backend')
+  , load_config = require('./config')
   , unpm = require('./lib/index')
   , path = require('path')
 
 module.exports = function(config) {
-  var data_dir = path.join(process.cwd(), 'data')
-    , unpm_service
+  var unpm_service
+    , tarballs_dir
+    , user_dir
+    , meta_dir
 
-  var tarballs_dir = path.join(data_dir, 'tarballs')
-    , user_dir = path.join(data_dir, 'users')
-    , meta_dir = path.join(data_dir, 'meta')
+  config = load_config(config || {})
+
+  tarballs_dir = path.join(config.data_dir, 'tarballs')
+  user_dir = path.join(config.data_dir, 'users')
+  meta_dir = path.join(config.data_dir, 'meta')
 
   config.backend = backend(meta_dir, user_dir, tarballs_dir)
 
