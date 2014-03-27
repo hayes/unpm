5b2cb50f7c3adda317cbfa552f6affa3907b146c
jesse keane <me@jessekeane.me>
2014-03-27 15:12:48 -0700

    move around files, change config

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
-var load_config = require('./lib/config')
-  , backend = require('unpm-fs-backend')
-  , unpm = require('./lib/index')
-  , path = require('path')
+var SessionStore = require('./lib/models/SessionStore')
+  , packages = require('./lib/controllers/packages')
+  , auth = require('./lib/controllers/auth')
+  , handler = require('./lib/handler')
+  , logging = require('./lib/logging')
+  , ns = require('./lib/context').ns
+  , Router = require('unpm-router')
+  , http = require('http')
 
-var CWD = process.cwd()
+module.exports = setup
 
-module.exports = function(config) {
-  var unpm_service
-    , tarballs_dir
-    , user_dir
-    , meta_dir
+function setup(config) {
+  return ns.run(function(context) {
+    unpm.call(context, ns, config)
+  })
+}
 
-  config = load_config(config || {})
+function unpm(ns, config) {
+  var router = Router(config.base_pathname)
 
-  if(!config.backend) {
-    tarballs_dir = path.join(CWD, 'tarballs')
-    user_dir = path.join(CWD, 'users')
-    meta_dir = path.join(CWD, 'meta')
+  handler = ns.bind(handler)
 
-    config.backend = backend(meta_dir, user_dir, tarballs_dir)
-  }
+  this.sessions = config.sessions || SessionStore()
+  this.server = http.createServer(handler)
+  this.log = logging(config)
+  this.port = config.port || 8123
+  this.backend = config.backend
+  this.handler = handler
+  this.router = router
+  this.config = config
 
-  unpm_service = unpm(config)
-  unpm_service.server.listen(unpm_service.config.port)
+  // auth
+  router.add('GET', routes.get_user, auth.user)
+  router.add('PUT', routes.register, auth.register)
+  router.add('PUT', routes.update_user, auth.update)
+  router.add('POST', routes.session, auth.sessions.create)
 
-  unpm_service.log.info('Started unpm on port %s', unpm_service.config.port)
+  // packages
+  router.add('PUT', routes.publish, packages.publish)
+  router.add('GET', routes.get_tarball, packages.get_tarball)
+  router.add('GET', routes.get_package, packages.get_package)
+
+  // clone from public npm
+  router.add('POST', routes.clone, packages.clone)
+  router.add('POST', routes.get_deps, packages.get_deps)
+}
+
+var routes = {
+    index: '/'
+  , get_package: '/:name/:version?'
+  , get_user: '/-/user/org.couchdb.user:*'
+  , get_tarball: '/:name/-/*-*.tgz'
+  , publish: '/:name'
+  , clone: '/clone/:name/:version?'
+  , get_deps: '/get_deps/:name/:version?'
+  , session: '/_session'
+  , register: '/-/user/org.couchdb.user:*'
+  , update_user: '/-/user/org.couchdb.user:*/*'
 }
