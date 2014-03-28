6debe93f111e3057ac961c63efbf7a20ca7373b5
AWinterman <andywinterman@gmail.com>
2014-03-27 21:38:00 -0700

    ironically, get tests in style

--- test/index.js
+++ test/index.js
@@ -3,6 +3,6 @@ var style = require('jsl/rules')
   , test = require('tape')
 
 test(
-      'code passes linter'
+    'code passes linter'
   , style.test([path.resolve(__dirname, '..', 'index.js')])
 )
