291aa12484a3cdb3c9e229e87643ee6805c3c801
Michael Hayes <michael.hayes@urbanairship.com>
2014-03-29 13:33:52 -0700

    added setup and teardown options to test

--- test/response.test.js
+++ test/response.test.js
@@ -12,101 +12,96 @@ test('verify 500', verify_500)
 function verify_json_response(t) {
   var res = {}
 
+  t.plan(3)
+
   res.writeHead = function(status, headers) {
     t.strictEqual(status, 200)
     t.deepEqual(headers, {'Content-Type': 'application/json'})
   }
 
-  res.write = function(data) {
-    t.strictEqual(data, '{"arbitrary": "data"}')
-  }
-
-  res.end = function() {
+  res.end = function(data) {
+    t.strictEqual(data, '{"arbitrary":"data"}')
     t.end()
   }
 
-  respond(null, res).json(200, '{"arbitrary": "data"}')
+  respond(null, res).json(200, {arbitrary: 'data'})
 }
 
-function verify_error_response(assert) {
+function verify_error_response(t) {
   var res = {}
 
+  t.plan(3)
+
   res.writeHead = function(status, headers) {
-    assert.strictEqual(status, 401)
-    assert.deepEqual(headers, {'Content-Type': 'application/json'})
+    t.strictEqual(status, 401)
+    t.deepEqual(headers, {'Content-Type': 'application/json'})
   }
 
-  res.write = function(data) {
-    assert.strictEqual(data, JSON.stringify({
+  res.end = function(data) {
+    t.strictEqual(data, JSON.stringify({
         error: 'unauthorized'
       , reason: 'Name or password is incorrect.'
     }))
-  }
-
-  res.end = function() {
-    assert.end()
+    t.end()
   }
 
   respond(null, res).unauthorized()
 }
 
-function verify_not_found(assert) {
+function verify_not_found(t) {
   var res = {}
 
+  t.plan(3)
+
   res.writeHead = function(status, headers) {
-    assert.strictEqual(status, 404)
-    assert.deepEqual(headers, {'Content-Type': 'application/json'})
+    t.strictEqual(status, 404)
+    t.deepEqual(headers, {'Content-Type': 'application/json'})
   }
 
-  res.write = function(data) {
-    assert.strictEqual(data, JSON.stringify({
+  res.end = function(data) {
+    t.strictEqual(data, JSON.stringify({
         error: 'not_found'
       , reason: 'Document not found.'
     }))
-  }
-
-  res.end = function() {
-    assert.end()
+    t.end()
   }
 
   respond(null, res).not_found()
 }
 
-function verify_conflict(assert) {
+function verify_conflict(t) {
   var res = {}
 
+  t.plan(3)
+
   res.writeHead = function(status, headers) {
-    assert.strictEqual(status, 409)
-    assert.deepEqual(headers, {'Content-Type': 'application/json'})
+    t.strictEqual(status, 409)
+    t.deepEqual(headers, {'Content-Type': 'application/json'})
   }
 
-  res.write = function(data) {
-    assert.strictEqual(data, JSON.stringify({
+  res.end = function(data) {
+    t.strictEqual(data, JSON.stringify({
         error: 'conflict'
       , reason: 'Document update conflict.'
     }))
-  }
-
-  res.end = function() {
-    assert.end()
+    t.end()
   }
 
   respond(null, res).conflict()
 }
 
-function verify_500(assert) {
+function verify_500(t) {
   var res = {}
 
+  t.plan(2)
+
   res.writeHead = function(status) {
-    assert.equal(status, 500)
+    t.equal(status, 500)
   }
 
-  res.write = function(data) {
-    assert.strictEqual('Something went wrong.', data)
-  }
-
-  res.end = function() {
-    assert.end()
+  res.end = function(data) {
+    t.strictEqual('Something went wrong.', data)
+    t.end()
   }
 
   respond(null, res).on_error(new Error)
