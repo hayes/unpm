ed965b03d598403163d49e5917f6a9ff281b948e
AWinterman <andywinterman@gmail.com>
2014-04-03 15:06:40 -0700

    updated

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var Package = require('./models/Package.test')
   , middleware = require('./middleware.test')
+  , integration = require('./integration')
   , response = require('./response.test')
   , handler = require('./handler.test')
-  , style = require('./style.test')
+/*   , style = require('./style.test') */
