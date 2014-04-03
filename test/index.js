6c68951b618eff88778c5d08dddc23f3035ae9f7
AWinterman <andywinterman@gmail.com>
2014-04-03 16:29:57 -0700

    removed integration tests from the process running unit tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,4 @@
-var integration = require('./integration/index.js')
-  , Package = require('./models/Package.test')
+var Package = require('./models/Package.test')
   , middleware = require('./middleware.test')
   , response = require('./response.test')
   , handler = require('./handler.test')
