a0fde74b294d42ff55fbe8a0c470a7311427f46d
jesse keane <me@jessekeane.me>
2016-04-13 13:11:19 -0700

    Add logging for CIDR failures

--- test/cidr.test.js
+++ test/cidr.test.js
@@ -1,4 +1,5 @@
 var unpm = require('../index')
+var cidr = require('../lib/cidr')
 var test = require('tape')
 
 var req = {}
@@ -76,3 +77,39 @@ test('allows if any range matches', function(t) {
     t.ok(true)
   })
 })
+
+test('logs when range check fails', function(t) {
+  var fakeUnpm = {
+    config: {cidr: ['10.0.0.0/8']},
+    log: {info: checkLog},
+    middleware: []
+  }
+  var check
+
+  t.plan(3)
+
+  cidr(fakeUnpm)
+  check = fakeUnpm.middleware[0]
+
+  check(
+    {req: req, res: {writeHead: checkHead, end: noop}},
+    null,
+    fakeUnpm,
+    function() {
+      t.fail('middleware should not call back')
+    }
+  )
+
+  function checkHead(code) {
+    t.equal(code, 403)
+  }
+
+  function checkLog(info) {
+    t.equal(info.ip, req.connection.remoteAddress)
+    t.true(info.message.indexOf('CIDR') > -1, 'message mentions CIDR')
+  }
+})
+
+function noop() {
+  // no-op
+}
